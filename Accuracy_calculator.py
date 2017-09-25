e7df48effb6717eaa8b5b95cd819b00e81cbf9a0
Buddhi Ayesha <buddhiayesha.13@cse.mrt.ac.lk>
2017-09-26 04:54:35 +0530

    Change code according to research paper

--- Accuracy_calculator.py
+++ Accuracy_calculator.py
@@ -1,80 +1,91 @@
-from gensim.models.keyedvectors import KeyedVectors
 import os
 
-# enter file input name
-mentionMap = 'finalMentionMap.txt'
+from gensim.models.keyedvectors import KeyedVectors
 
-# enter word2vec bin file name
-vectorFileName = 'word2vec.txt'
 
-# get Current path of this file
-currentPath = os.getcwd()
+def clculate(mentionMapPath, vectorFilePath):
+    # enter file input name
+    mentionMap = 'MentionMap.txt'
 
-# load word2vec file. Check whether the file is a Binary or not and set the boolean accordingly
-model = KeyedVectors.load_word2vec_format(currentPath + '/' + vectorFileName, binary=False)
+    # enter word2vec bin file name
+    vectorFileName = 'DocVector_in_word2vec.txt'
 
-# Start reading the file in separate lines
-with open(currentPath + '/' + mentionMap) as f:
-    lines = f.readlines()
-    # stripping the newline character
-    lines = [x.strip() for x in lines]
+    # load word2vec file. Check whether the file is a Binary or not and set the boolean accordingly
+    model = KeyedVectors.load_word2vec_format(os.path.join(vectorFilePath, vectorFileName), binary=False)
 
-# set the number of similar documents expected to return from the word2vec file
-# use the maximum reference document count for a given document in the mention map
-max = 0
-for line in lines:
-    if (';') in line:
-        document = line.split(';')[0]
-        referencesList = line.split(';')[1].split(',')
-        if max < len(referencesList):
-            max = len(referencesList)
-outputCount = 100
+    # Start reading the file in separate lines
+    with open(os.path.join(mentionMapPath, mentionMap)) as f:
+        lines = f.readlines()
+        # stripping the newline character
+        lines = [x.strip() for x in lines]
 
-# initialize for precision and recall
-precision = 0.0
-recall = 0.0
+    # set the number of similar documents expected to return from the word2vec file
+    # use the maximum reference document count for a given document in the mention map
+    max = 0
+    for line in lines:
+        if (';') in line:
+            document = line.split(';')[0]
+            referencesList = line.split(';')[1].split(',')
+            if max < len(referencesList):
+                max = len(referencesList)
+    outputCount = 100
 
-# a count varianle to get visual feedback on the document count when running program
-printCounter = 0
-notInDictionary = 0
+    # initialize for precision and recall
+    precision = 0.0
+    recall = 0.0
 
-# loop through each document in the mention map
-for line in lines:
+    # a count varianle to get visual feedback on the document count when running program
+    printCounter = 0
+    notInDictionary = 0
 
-    # if semicolon is not there, there are no reference
-    if (';') not in line:
-        pass
+    # loop through each document in the mention map
+    for line in lines:
 
-    else:
-        document = line.split(';')[0]
-        referencesList = line.split(';')[1].split(',')
+        # if semicolon is not there, there are no reference
+        if (';') not in line:
+            pass
 
-        try:
-            modelOutput = model.most_similar(positive=[document], topn=outputCount)
+        else:
+            document = line.split(';')[0]
+            referencesList = line.split(';')[1].split(',')
 
-        except KeyError:
-            print document + " not in dictionary"
-            notInDictionary = notInDictionary -1
-            printCounter = printCounter + 1
-            continue
-        
-        # create a list of the documents only, returned by the model. Remove the vector values
-        documentList = []
-        for i in range(0, outputCount):
-            documentList.append(modelOutput[i][0])
+            try:
+                modelOutput = model.most_similar(positive=[document], topn=outputCount)
 
-        intersectionCount = len(list(set(documentList).intersection(referencesList)))
+            except KeyError:
+                print document + " not in dictionary"
+                notInDictionary = notInDictionary - 1
+                printCounter = printCounter + 1
+                continue
 
-        precision += float(intersectionCount) / float(outputCount)
-        recall += float(intersectionCount) / float(100)
+            # create a list of the documents only, returned by the model. Remove the vector values
+            documentList = []
+            for i in range(0, outputCount):
+                documentList.append(modelOutput[i][0])
 
-    print str(printCounter) + '/' + str(len(lines)) + ' : Pre = ' + str(precision) + ' / Recall = ' + str(recall)
-    printCounter = printCounter + 1
+            intersectionCount = len(list(set(documentList).intersection(referencesList)))
 
-# calculate the final value for precision and recall
-precision = float(precision) / float(len(lines)-notInDictionary)
-recall = float(recall) / float(len(lines)-notInDictionary)
+            precision += float(intersectionCount) / float(outputCount)
+            recall += float(intersectionCount) / float(100)
 
-print "Precision = " + str(precision*100)
-print "Recall = " + str(recall*100)
+        # print str(printCounter) + '/' + str(len(lines)) + ' : Pre = ' + str(precision) + ' / Recall = ' + str(recall)
+        printCounter = printCounter + 1
 
+    # calculate the final value for precision and recall
+    precision = float(precision) / float(len(lines) - notInDictionary)
+    recall = float(recall) / float(len(lines) - notInDictionary)
+
+    print "Precision = " + str(precision * 100),
+    print "Recall = " + str(recall * 100)
+
+
+alpha = ["a0.1", "a0.5", "a1", "a2", "a3"]
+p_value = ["p250", "p500", "p750", "p1000"]
+
+for a in alpha:
+    for p in p_value:
+        print "Alpha: " + a + "\tP value: " + p,
+        mentionMapPath = os.path.join("D:\Project", "fyp\word2vec\code\work12\cbwc\work1\drive")
+        vectorFilePath = os.path.join("D:\Project", "fyp\word2vec\code\work12\cbwc\work1\drive\Serialized_folder",
+                                      a, p)
+        clculate(mentionMapPath, vectorFilePath)
